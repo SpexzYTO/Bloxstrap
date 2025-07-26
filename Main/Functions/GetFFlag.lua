2dc62c2c584e7e89ae0bf5f91b563a0df5e20074
SpexzYTO <84864497+SpexzYTO@users.noreply.github.com>
2025-07-26 01:06:05 -0700

    GetFFlag.lua

--- Main/Functions/GetFFlag.lua
+++ Main/Functions/GetFFlag.lua
@@ -1,10 +1,43 @@
-return function(flag: string, value: string?): (string, string?) -> (string?)
-	if type(flag) ~= "string" then return task.spawn(error, "string expected, got "..type(flag)) end
-	local FFlag: string = Bloxstrap.TouchEnabled and flag:gsub("DFInt", ""):gsub("DFFlag", ""):gsub("FFlag", ""):gsub("FInt", ""):gsub("DFString", ""):gsub("FString", "") :gsub("FLog", ""):gsub("DFLog", ""):gsub("DFFLoat", ""):gsub("FFloat", ""):gsub("DFTime", ""):gsub("FTime", ""):gsub("DFTest", ""):gsub("FTest", ""):gsub("DFUInt", ""):gsub("FUInt", ""):gsub("SFInt", ""):gsub("SFFlag", "") or flag --> Removes the keyword of the FFlag, setfflag doesn't like those so we will need to remove it.
-	
-	if getfflag(FFlag) ~= nil then
-		return getfflag(FFlag)
+local cloneref = cloneref or function(...) return ... end
+local HttpService = cloneref(game:GetService("HttpService"))
+
+return function(flag: string, value: string?): (string?)
+	if type(flag) ~= "string" then
+		return task.spawn(error, "string expected, got " .. type(flag))
+	end
+
+	local FFlag: string = Bloxstrap.TouchEnabled and flag
+		:gsub("DFInt", "")
+		:gsub("DFFlag", "")
+		:gsub("FFlag", "")
+		:gsub("FInt", "")
+		:gsub("DFString", "")
+		:gsub("FString", "")
+		:gsub("FLog", "")
+		:gsub("DFLog", "")
+		:gsub("DFFLoat", "")
+		:gsub("FFloat", "")
+		:gsub("DFTime", "")
+		:gsub("FTime", "")
+		:gsub("DFTest", "")
+		:gsub("FTest", "")
+		:gsub("DFUInt", "")
+		:gsub("FUInt", "")
+		:gsub("SFInt", "")
+		:gsub("SFFlag", "")
+		or flag
+
+	local path = "Bloxstrap/FFlags.json"
+	local flags = isfile(path) and HttpService:JSONDecode(readfile(path)) or {}
+
+	local success, result = pcall(getfflag, FFlag)
+
+	if success and result ~= nil then
+		return result
 	else
-		return task.spawn(error, "FFlag expected, got "..FFlag)
+		warn("⚠️ getfflag failed for:", FFlag)
+		flags[flag] = nil
+		writefile(path, HttpService:JSONEncode(flags))
+		return nil
 	end
 end
